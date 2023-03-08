75448ce471533b28a7128f5a8310368171a956ef
Zano <zanopcwr@protonmail.com>
2023-03-08 12:32:33 -0300

    Added main file

---
+++ ProcImg-TDE-1/ProcImg-TDE-1/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+float Normalize_RGB(int iR, int iG, int iB)
+{
+
+
+
+    return 0.0f;
+}
+
+Console.WriteLine("Hello, World!");
+
+
