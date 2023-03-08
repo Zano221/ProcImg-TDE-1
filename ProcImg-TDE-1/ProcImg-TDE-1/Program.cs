ee1d5c61781057c7fcc4afdbb38ce6180c82fce5
Zano <zanopcwr@protonmail.com>
2023-03-08 16:23:04 -0300

    Fixed RGB to CMYK not working, and added early RGB to HSV function todo tomorrow

--- ProcImg-TDE-1/ProcImg-TDE-1/Program.cs
+++ ProcImg-TDE-1/ProcImg-TDE-1/Program.cs
@@ -1,4 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System;
 
 (int, int, int) Assert_RGB_8Bits(int iR, int iG, int iB)
 {
@@ -17,7 +18,7 @@
 
 float Normalize_RGB(int iR, int iG, int iB)
 {
-    (iR, iG, iB) = Assert_RGB_8Bits(iR, iG, iB);
+    if (iR == 0 && iG == 0 && iB == 0) return 0;
 
     float fR = Convert.ToSingle(iR);
     float fG = Convert.ToSingle(iG);
@@ -30,21 +31,90 @@ float Normalize_RGB(int iR, int iG, int iB)
     return fCalculatedR + fCalculatedG + fCalculatedB;
 }
 
-(int ,int ,int ,int) Convert_RGB_CMYK(int iR, int iG, int iB)
+(float , float, float, float) Convert_RGB_CMYK(int iR, int iG, int iB)
 {
+
+    float iCalculatedR = Convert.ToSingle(iR) / 255;
+    float iCalculatedG = Convert.ToSingle(iG) / 255;
+    float iCalculatedB = Convert.ToSingle(iB) / 255;
+
+    if (iCalculatedR == 0 && iCalculatedG == 0 && iCalculatedB == 0) return (0, 0, 0, 1);
+
+    Console.WriteLine(iCalculatedR + iCalculatedG + iCalculatedB);
+
+    float iK = 1.0f - Math.Max(Math.Max(iCalculatedR, iCalculatedG), iCalculatedB);
+
+    Console.WriteLine("K é = " + iK);
+
+    Console.WriteLine("CONVERTENDO PARA VRAU");
+    Console.WriteLine("1 - " + iCalculatedR + " - " + iK + " / " + "(1 - " + iK + ")");
+    float iC = (1 - iCalculatedR - iK) / (1 - iK);
+    float iM = (1 - iCalculatedG - iK) / (1 - iK);
+    float iY = (1 - iCalculatedB - iK) / (1 - iK);
+
+    return (iC * 100, iM * 100, iY * 100, iK * 100);
 
 }
 
-Console.Write("Insira o valor R: ");
-int iInputR = Convert.ToInt32(Console.ReadLine());
+(int, int, int) Convert_CMYK_RGB(int iC, int iM, int iY, int iK)
+{
+    int iR = 255 * (1 - iC) * (1 - iK);
+    int iG = 255 * (1 - iM) * (1 - iK);
+    int iB = 255 * (1 - iY) * (1 - iK);
 
-Console.Write("Insira o valor G: ");
-int iInputG = Convert.ToInt32(Console.ReadLine());
-
-Console.Write("Insira o valor B: ");
-int iInputB = Convert.ToInt32(Console.ReadLine());
+    return (iR, iG, iB);
+}
 
 
-Console.WriteLine("Valor RGB Normalizado: " + Normalize_RGB(iInputR, iInputG, iInputB).ToString());
+(float, float, float) Convert_RGB_HSV(int iR, int iG, int iB)
+{
+    return (0, 0, 0);
+}
+
+
+String sInput = " ";
+
+while(sInput != "\n")
+{
+    Console.Write("Insira o valor R: ");
+    int iInputR = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("Insira o valor G: ");
+    int iInputG = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("Insira o valor B: ");
+    int iInputB = Convert.ToInt32(Console.ReadLine());
+
+
+    (iInputR, iInputG, iInputB) = Assert_RGB_8Bits(iInputR, iInputG, iInputB);
+
+    Console.WriteLine("");
+    Console.WriteLine("Valor RGB Normalizado: " + Normalize_RGB(iInputR, iInputG, iInputB).ToString());
+
+    String sCOutput;
+    String sYOutput;
+    String sMOutput;
+    String sKOutput;
+
+    float fCOutput;
+    float fYOutput;
+    float fMOutput;
+    float fKOutput;
+
+
+    (fCOutput, fYOutput, fMOutput, fKOutput) = Convert_RGB_CMYK(iInputR, iInputG, iInputB);
+
+    sCOutput = fCOutput.ToString("f1");
+    sYOutput = fYOutput.ToString("f1");
+    sMOutput = fMOutput.ToString("f1");
+    sKOutput = fKOutput.ToString("f1");
+
+
+    Console.WriteLine("Valor RGB Convertido para CMYK: (" + sCOutput + "% , " + sYOutput + "% , " +  sMOutput + "% , " + sKOutput + "%)" );
+
+    Console.WriteLine("Valor RGB Convertido para HSV: ");
+}
+
+
 
 
