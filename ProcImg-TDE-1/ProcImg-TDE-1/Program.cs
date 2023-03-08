d62f6099ca8ec32b8cf3b364135cde0f613070e8
Zano <zanopcwr@protonmail.com>
2023-03-08 13:07:28 -0300

    Improved some code beauty in Assert_RGB_8Bits function

--- ProcImg-TDE-1/ProcImg-TDE-1/Program.cs
+++ ProcImg-TDE-1/ProcImg-TDE-1/Program.cs
@@ -3,37 +3,14 @@
 (int, int, int) Assert_RGB_8Bits(int iR, int iG, int iB)
 {
     //Maior que 8bitos
-    if(iR > 255)
-    {
-        iR = 255;
-    }
-
-    if(iG > 255)
-    {
-        iG = 255;
-    }
-
-    if(iB > 255)
-    {
-        iB = 255;
-    }
+    if(iR > 255) iR = 255;
+    if(iG > 255) iG = 255;
+    if(iB > 255) iB = 255;
 
     //Menor que 8bitos
-
-    if(iR < 0)
-    {
-        iR = 0;
-    }
-
-    if(iG < 0)
-    {
-        iG = 0;
-    }
-
-    if(iB < 0)
-    {
-        iB = 0;
-    }
+    if(iR < 0) iR = 0;
+    if(iG < 0) iG = 0;
+    if(iB < 0) iB = 0;
 
     return (iR, iG, iB);
 }
@@ -42,11 +19,20 @@ float Normalize_RGB(int iR, int iG, int iB)
 {
     (iR, iG, iB) = Assert_RGB_8Bits(iR, iG, iB);
 
-    float fCalculatedR = (float)iR / (iR + iG + iB);
-    float fCalculatedG = (float)iG / (iR + iG + iB);
-    float fCalculatedB = (float)iB / (iR + iG + iB);
+    float fR = Convert.ToSingle(iR);
+    float fG = Convert.ToSingle(iG);
+    float fB = Convert.ToSingle(iB);
+
+    float fCalculatedR = fR / (fR + fG + fB);
+    float fCalculatedG = fG / (fR + fG + fB);
+    float fCalculatedB = fB / (fR + fG + fB);
 
     return fCalculatedR + fCalculatedG + fCalculatedB;
+}
+
+(int ,int ,int ,int) Convert_RGB_CMYK(int iR, int iG, int iB)
+{
+
 }
 
 Console.Write("Insira o valor R: ");
