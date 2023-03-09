834bd9ec756a225f5d07f987499408b40a8a041b
Zano <zanopcwr@protonmail.com>
2023-03-09 18:16:46 -0300

    feat: Added Calc_Delta function (necessary???) and finished Convert_RGB_HSV function (didn't test it yet).

--- ProcImg-TDE-1/ProcImg-TDE-1/Program.cs
+++ ProcImg-TDE-1/ProcImg-TDE-1/Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System;
+using System.Diagnostics;
 
 (int, int, int) Assert_RGB_8Bits(int iR, int iG, int iB)
 {
@@ -14,6 +15,18 @@ using System;
     if(iB < 0) iB = 0;
 
     return (iR, iG, iB);
+}
+
+float Calc_Delta(int iR, int iG, int iB)
+{
+    float iCalculatedR = Convert.ToSingle(iR) / 255;
+    float iCalculatedG = Convert.ToSingle(iG) / 255;
+    float iCalculatedB = Convert.ToSingle(iB) / 255;
+
+    float fMax = Math.Max(Math.Max(iCalculatedR, iCalculatedG), iCalculatedB);
+    float fMin = Math.Min(Math.Min(iCalculatedR, iCalculatedG), iCalculatedB);
+
+    return fMax - fMin;
 }
 
 float Normalize_RGB(int iR, int iG, int iB)
@@ -34,25 +47,25 @@ float Normalize_RGB(int iR, int iG, int iB)
 (float , float, float, float) Convert_RGB_CMYK(int iR, int iG, int iB)
 {
 
-    float iCalculatedR = Convert.ToSingle(iR) / 255;
-    float iCalculatedG = Convert.ToSingle(iG) / 255;
-    float iCalculatedB = Convert.ToSingle(iB) / 255;
+    float fCalculatedR = Convert.ToSingle(iR) / 255;
+    float fCalculatedG = Convert.ToSingle(iG) / 255;
+    float fCalculatedB = Convert.ToSingle(iB) / 255;
 
-    if (iCalculatedR == 0 && iCalculatedG == 0 && iCalculatedB == 0) return (0, 0, 0, 1);
+    if (fCalculatedR == 0 && fCalculatedG == 0 && fCalculatedB == 0) return (0, 0, 0, 1);
 
-    Console.WriteLine(iCalculatedR + iCalculatedG + iCalculatedB);
+    float fK = 1.0f - Math.Max(Math.Max(fCalculatedR, fCalculatedG), fCalculatedB);
 
-    float iK = 1.0f - Math.Max(Math.Max(iCalculatedR, iCalculatedG), iCalculatedB);
+    
 
-    Console.WriteLine("K é = " + iK);
+    Console.WriteLine("K é = " + fK);
 
     Console.WriteLine("CONVERTENDO PARA VRAU");
-    Console.WriteLine("1 - " + iCalculatedR + " - " + iK + " / " + "(1 - " + iK + ")");
-    float iC = (1 - iCalculatedR - iK) / (1 - iK);
-    float iM = (1 - iCalculatedG - iK) / (1 - iK);
-    float iY = (1 - iCalculatedB - iK) / (1 - iK);
+    Console.WriteLine("1 - " + fCalculatedR + " - " + fK + " / " + "(1 - " + fK + ")");
+    float iC = (1 - fCalculatedR - fK) / (1 - fK);
+    float iM = (1 - fCalculatedG - fK) / (1 - fK);
+    float iY = (1 - fCalculatedB - fK) / (1 - fK);
 
-    return (iC * 100, iM * 100, iY * 100, iK * 100);
+    return (iC * 100, iM * 100, iY * 100, fK * 100);
 
 }
 
@@ -68,25 +81,79 @@ float Normalize_RGB(int iR, int iG, int iB)
 
 (float, float, float) Convert_RGB_HSV(int iR, int iG, int iB)
 {
-    return (0, 0, 0);
+    float fDelta = Calc_Delta(iR, iG, iB);
+
+    float fCalculatedR = Convert.ToSingle(iR) / 255;
+    float fCalculatedG = Convert.ToSingle(iG) / 255;
+    float fCalculatedB = Convert.ToSingle(iB) / 255;
+
+
+    float fMax = Math.Max(Math.Max(fCalculatedR, fCalculatedG), fCalculatedB);
+    float fMin = Math.Min(Math.Min(fCalculatedR, fCalculatedG), fCalculatedB);
+
+    float fH = 0;
+
+
+    //Calcular o Hue
+    if (fMax == fCalculatedR)
+    {
+        if(fCalculatedG >= fCalculatedB)
+        {
+            fH = 60 * (fCalculatedG - fCalculatedB) / fDelta;
+        }
+        else if(fCalculatedG < fCalculatedB)
+        {
+            fH = 60 * ((fCalculatedG - fCalculatedB) / fDelta) + 360;
+        }
+    }
+    else if(fMax == fCalculatedG)
+    {
+        fH = 60 * ((fCalculatedB - fCalculatedR) / fDelta) + 120;
+    }
+    else if(fMax == fCalculatedB)
+    {
+        fH = 60 * ((fCalculatedR - fCalculatedG) / fDelta) + 240;
+    }
+
+    //Calcular a Saturação
+    float fS = fDelta / fMax;
+
+    //Calcular o Valor
+    float fV = fMax;
+
+    return (fH, fS, fV);
 }
 
 
-String sInput = " ";
+bool bIsInput = true;
 
-while(sInput != "\n")
+while(true)
 {
-    Console.Write("Insira o valor R: ");
-    int iInputR = Convert.ToInt32(Console.ReadLine());
+    int iInputR = 0, iInputG = 0, iInputB = 0;
+    try
+    {
+        Console.Write("Insira o valor R: ");
+        iInputR = Convert.ToInt32(Console.ReadLine());
 
-    Console.Write("Insira o valor G: ");
-    int iInputG = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Insira o valor G: ");
+        iInputG = Convert.ToInt32(Console.ReadLine());
 
-    Console.Write("Insira o valor B: ");
-    int iInputB = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Insira o valor B: ");
+        iInputB = Convert.ToInt32(Console.ReadLine());
 
+        (iInputR, iInputG, iInputB) = Assert_RGB_8Bits(iInputR, iInputG, iInputB);
+    }
+    catch(Exception e)
+    {
+        Console.WriteLine("\n" +
+            "   --- ERRO!, o valor inserido não é numerico! --- \n");
+        bIsInput = false;
+    }
 
-    (iInputR, iInputG, iInputB) = Assert_RGB_8Bits(iInputR, iInputG, iInputB);
+    if(!bIsInput)
+    {
+        return;
+    }
 
     Console.WriteLine("");
     Console.WriteLine("Valor RGB Normalizado: " + Normalize_RGB(iInputR, iInputG, iInputB).ToString());
@@ -114,6 +181,9 @@ while(sInput != "\n")
 
     Console.WriteLine("Valor RGB Convertido para HSV: ");
 }
+
+
+
 
 
 
